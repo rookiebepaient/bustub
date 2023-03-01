8e18dcb01eacb1ddd375b424c396dc27edf1874e
rookiebepaient <chen18249881210@163.com>
2023-03-01 15:21:00 +0800

    src/planner/plan_func_call.cpp 未完成tast4

--- src/planner/plan_func_call.cpp
+++ src/planner/plan_func_call.cpp
@@ -39,7 +39,19 @@ auto Planner::GetFuncCallFromFactory(const std::string &func_name, std::vector<A
   // 1. check if the parsed function name is "lower" or "upper".
   // 2. verify the number of args (should be 1), refer to the test cases for when you should throw an `Excepetion`.
   // 3. return a `StringExpression` std::shared_ptr.
-  throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
+  //throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
+
+  AbstractExpressionRef ret = nullptr;
+  if (func_name == "lower" || func_name == "upper") {
+    if (args.size() == 1) {
+      if (args.at(0)->GetChildAt(0)->GetReturnType() != TypeId::VARCHAR) {
+        throw bustub::NotImplementedException("expect the first arg to be varchar");
+      } else {
+        ret = args.at(0);
+      }
+    }
+  }
+  return ret;
 }
 
 }  // namespace bustub
